d05f794ab1a7b341bcd75fbbafba9e9ba53d9aa4
Zane Manning <greenzanman@gmail.com>
2025-08-26 11:59:33 -0400

    add pretty_assertions

--- projects/tailwind-rs/tests/html/mod.rs
+++ projects/tailwind-rs/tests/html/mod.rs
@@ -1,6 +1,11 @@
 use tailwind_css_fixes::{CssInlineMode, TailwindBuilder};
 use tailwind_rs::CLIConfig;
 
+// the pretty_assertions crate will show diff in terminal, if the outputs differ.
+#[cfg(test)]
+use pretty_assertions::assert_eq as assert_eq_diff;
+
+
 mod accessibility;
 mod arbitrary;
 mod background;
